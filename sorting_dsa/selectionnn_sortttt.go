2a939c074b872020a5dd4291a7dc6a9f95dd8f65
mubashir6822 <119034258+mubashir6822@users.noreply.github.com>
2022-11-29 15:20:24 +0530

    Add files via upload

---
+++ sorting_dsa/selectionnn_sortttt.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func selectionSort(numbers []int) []int {
+	var mid_index int
+	for i := 0; i < len(numbers); i++ {
+		mid_index = i
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[j] < numbers[mid_index] {
+				mid_index = j
+			}
+		}
+		temp := numbers[mid_index]
+		numbers[mid_index] = numbers[i]
+		numbers[i] = temp
+	}
+	return numbers
+}
+
+// main function
+func main() {
+	fmt.Println("Selection Sort \n")
+	// creating an integer array with values
+	number := []int{2, 1, 4, 6, 11, 5, 3, 8, 12, 7, 10, 9}
+	fmt.Println("Before Sorting:", number)
+	// calling the selectionSort function
+	number = selectionSort(number)
+	fmt.Println("\nAfter Sorting:", number)
+}
